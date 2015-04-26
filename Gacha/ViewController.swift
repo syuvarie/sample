a0a48374d124d285bd908f6bf77618f6c4ee3323
奥田紘士 <syuvarie@okudahiroshishi-no-MacBook-Pro.local>
2015-04-26 15:39:32 +0900

    Initial Commit

---
+++ Gacha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gacha
+//
+//  Created by 奥田紘士 on 2015/04/26.
+//  Copyright (c) 2015年 奥田紘士. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GachaTests/GachaTests.swift
@@ -0,0 +1,36 @@
+//
+//  GachaTests.swift
+//  GachaTests
+//
+//  Created by 奥田紘士 on 2015/04/26.
+//  Copyright (c) 2015年 奥田紘士. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GachaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
